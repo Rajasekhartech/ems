7f641ef7838cf670360f98bb3e33234e265c6c2f
unknown <rajasekhartechpy@gmail.com>
2020-01-22 16:47:14 +0530

    added poll details

--- poll/views.py
+++ poll/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404 , redirect
+from django.http import Http404, HttpResponse, HttpResponseRedirect, JsonResponse
 from poll.models import *
 from django.http import Http404 , HttpResponse
 from  django.contrib.auth.decorators import login_required
+from ems.decorators import admin_hr_required
+from django.views.generic import View
+from django.utils.decorators import method_decorator
+from poll.forms import PollForm, ChoiceForm
 # Create your views here.
 
 @login_required(login_url= '/login/')
@@ -42,3 +47,68 @@ def poll(request, id = None):
             return HttpResponse("Your vote is done succesfully")
         else:
             return HttpResponse("Your vote is not done sucessfully")
+
+class PollView(View):
+    decorators = [login_required, admin_hr_required]
+
+    @method_decorator(decorators)
+    def get(self, request, id=None):
+        if id:
+            question = get_object_or_404(Question, id=id)
+            poll_form = PollForm(instance=question)
+            choices = question.choice_set.all()
+            choice_forms = [ChoiceForm(prefix=str(
+                choice.id), instance=choice) for choice in choices]
+            template = 'polls/edit_poll.html'
+        else:
+            poll_form = PollForm(instance=Question())
+            choice_forms = [ChoiceForm(prefix=str(
+                x), instance=Choice()) for x in range(3)]
+            template = 'polls/new_poll.html'
+        context = {'poll_form': poll_form, 'choice_forms': choice_forms}
+        return render(request, template, context)
+
+    @method_decorator(decorators)
+    def post(self, request, id=None):
+        context = {}
+        if id:
+            return self.put(request, id)
+        poll_form = PollForm(request.POST, instance=Question())
+        choice_forms = [ChoiceForm(request.POST, prefix=str(
+            x), instance=Choice()) for x in range(0, 3)]
+        if poll_form.is_valid() and all([cf.is_valid() for cf in choice_forms]):
+            new_poll = poll_form.save(commit=False)
+            new_poll.created_by = request.user
+            new_poll.save()
+            for cf in choice_forms:
+                new_choice = cf.save(commit=False)
+                new_choice.question = new_poll
+                new_choice.save()
+            return HttpResponseRedirect('/poll/list/')
+        context = {'poll_form': poll_form, 'choice_forms': choice_forms}
+        return render(request, 'polls/new_poll.html', context)
+
+    @method_decorator(decorators)
+    def put(self, request, id=None):
+        context = {}
+        question = get_object_or_404(Question, id=id)
+        poll_form = PollForm(request.POST, instance=question)
+        choice_forms = [ChoiceForm(request.POST, prefix=str(
+            choice.id), instance=choice) for choice in question.choice_set.all()]
+        if poll_form.is_valid() and all([cf.is_valid() for cf in choice_forms]):
+            new_poll = poll_form.save(commit=False)
+            new_poll.created_by = request.user
+            new_poll.save()
+            for cf in choice_forms:
+                new_choice = cf.save(commit=False)
+                new_choice.question = new_poll
+                new_choice.save()
+            return redirect('polls_list')
+        context = {'poll_form': poll_form, 'choice_forms': choice_forms}
+        return render(request, 'polls/edit_poll.html', context)
+
+    @method_decorator(decorators)
+    def delete(self, request, id=None):
+        question = get_object_or_404(Question)
+        question.delete()
+        return redirect('polls_list')
